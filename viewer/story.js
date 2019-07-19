cdc0e6f1e26b49a80099fcfe2ab8d616af4848e3
Jose.Glas <48224956+JoseGlasOLX@users.noreply.github.com>
2019-07-19 12:58:46 -0300

    Primer comment
    cambios

---
+++ viewer/story.js
@@ -0,0 +1,282 @@
+var story = {
+"docName": "ui-landing-urban-credit",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": true,
+"disableHotspots": true,
+"pages": [
+$.extend(new ViewerPage(),{
+"index": 0,
+"image": "desktop_home.png",
+"image2x": "desktop_home@2x.png",
+"width": 1336,
+"height": 1220,
+"title": "Desktop - home",
+'layout' : {
+	"offset": 240,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 413,
+			"y": 584,
+			"width": 243,
+			"height": 243
+		},
+		"isParentFixed": false,
+		"page": 1,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 680,
+			"y": 584,
+			"width": 243,
+			"height": 243
+		},
+		"isParentFixed": false,
+		"page": 4,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 1,
+"image": "desktop_landing_paso_1.png",
+"image2x": "desktop_landing_paso_1@2x.png",
+"width": 1440,
+"height": 1110,
+"title": "Desktop - landing - paso 1",
+'layout' : {
+	"offset": 240,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 88,
+			"y": 832,
+			"width": 540,
+			"height": 56
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 2,
+"image": "desktop_landing_paso_2.png",
+"image2x": "desktop_landing_paso_2@2x.png",
+"width": 1440,
+"height": 1499,
+"title": "Desktop - landing - paso 2",
+'layout' : {
+	"offset": 240,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 88,
+			"y": 1288,
+			"width": 540,
+			"height": 135
+		},
+		"isParentFixed": false,
+		"page": 3,
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 3,
+"image": "desktop_landing_paso_3.png",
+"image2x": "desktop_landing_paso_3@2x.png",
+"width": 1440,
+"height": 1110,
+"title": "Desktop - landing - paso 3",
+'layout' : {
+	"offset": 240,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"index": 4,
+"image": "mobile_landing_paso_1.png",
+"image2x": "mobile_landing_paso_1@2x.png",
+"width": 414,
+"height": 1190,
+"title": "Mobile - landing - paso 1",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 1,
+		"y": 0,
+		"width": 414,
+		"height": 24,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "mobile_landing_paso_1-0.png",
+		"image2x": "mobile_landing_paso_1-0@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 16.997389033942568,
+			"y": 881,
+			"width": 381.0026109660573,
+			"height": 56
+		},
+		"isParentFixed": false,
+		"page": 5,
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 5,
+"image": "mobile_landing_paso_2.png",
+"image2x": "mobile_landing_paso_2@2x.png",
+"width": 414,
+"height": 1880,
+"title": "Mobile - landing - paso 2",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 1,
+		"y": 0,
+		"width": 414,
+		"height": 24,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "mobile_landing_paso_2-0.png",
+		"image2x": "mobile_landing_paso_2-0@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 18,
+			"y": 1523,
+			"width": 380,
+			"height": 56
+		},
+		"isParentFixed": false,
+		"page": 6,
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 6,
+"image": "mobile_landing_paso_3.png",
+"image2x": "mobile_landing_paso_3@2x.png",
+"width": 414,
+"height": 746,
+"title": "Mobile - landing - paso 3",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 1,
+		"y": 0,
+		"width": 414,
+		"height": 24,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "mobile_landing_paso_3-0.png",
+		"image2x": "mobile_landing_paso_3-0@2x.png"
+	}
+],
+'links' : [],
+})
+   ]
+,"resolutions": [2],
+"zoomEnabled": true,
+"title": "ui-landing-Urban Credit",
+"layersExist": true,
+"centerContent":  false,
+"totalImages": 10,
+"highlightLinks": false
+}
